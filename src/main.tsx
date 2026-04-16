488d926edbd409147aef0bdda8d7e9dda3aab0a4
capital0707-design <capital0707@gmail.com>
2026-04-16 21:20:04 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Проверка связи!</h1>
     <App />
   </StrictMode>
 );
