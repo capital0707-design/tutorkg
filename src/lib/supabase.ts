6cb07c627c5f612737ab1f6ff6624dc33345faa7
capital0707-design <capital0707@gmail.com>
2026-04-23 22:41:53 +0600

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,4 @@ import type { Database } from './database.types';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(https://gbcjbslclauymjccekbr.supabase.co, NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_dsJ6MgWUbGskTjWvxcg3Uw_pQKZ0E_a);
